60024ada0ef9d0ad7fb6f8a4bf27fd1152501767
Leonardo Suguino <leonardo_suguino@hotmail.com>
2025-01-08 21:51:48 -0300

    Puxando de amanda

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ from tkinter import *
 from tkinter import ttk
 
 #### cores ####
-co0 = "444466" #Preto
-co1 = "feffff" #Branco
-co2 = "6f9fbd" #Azul
-co3 = "38576b" #Valor
-co4 = "403d3d" #Letra
-co5 = "ef5350" #Vermelho
+co0 = "#444466" #Preto
+co1 = "#feffff" #Branco
+co2 = "#6f9fbd" #Azul
+co3 = "#38576b" #Valor
+co4 = "#403d3d" #Letra
+co5 = "#ef5350" #Vermelho
 
 #Criando janela
 janela = Tk()
 janela.title('')
-janela.geometry('1080x720')
+janela.geometry('550x510')
+janela.configure(bg=co5)
 #janela.configure (bg = co1)
-#Oi
 
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=0,columnspan=1,ipadx=272)
 
 style = ttk.Style(janela)
 style.theme_use("clam")
+
+#Criando frame
+frame_pokemon = Frame(janela, width = 550, height = 290, relief = 'flat')
+frame_pokemon.grid(row = 1, column = 0)
 
 
 janela.mainloop()
