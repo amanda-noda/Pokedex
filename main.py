3e339c2c5d6dffb5b79971995a4a232aaa8f0dde
Amanda Fernandes Cutódio <58089751+amanda-noda@users.noreply.github.com>
2025-01-08 21:40:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,21 @@
 from tkinter import *
 from tkinter import ttk
 
-#### cores ####
-co0 = "444466" #Preto
-co1 = "feffff" #Branco
-co2 = "6f9fbd" #Azul
-co3 = "38576b" #Valor
-co4 = "403d3d" #Letra
-co5 = "ef5350" #Vermelho
 
-#Criando janela
+##Cria a janela
 janela = Tk()
 janela.title('')
-janela.geometry('1080x720')
-#janela.configure (bg = co1)
-#Oi
+janela.geometry('550x510')
+janela.configure(bg='white')
 
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=0,columnspan=1,ipadx=272)
 
 style = ttk.Style(janela)
 style.theme_use("clam")
+
+##Cria Frame
+frame_pokemon = Frame(janela, width=550, height=290, relief='flat')
+frame_pokemon.grid(row=1, column=0)
 
 
 janela.mainloop()
