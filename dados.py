f0e64f30d515aec1b86eb80f5526a7d95da67772
Leonardo Suguino <leonardo_suguino@hotmail.com>
2025-01-13 15:30:32 -0300

    Update on Dados

--- dados.py
+++ dados.py
@@ -8,19 +8,19 @@ pokemon = {
 
     'aerodactyl': {
         'status':['Hp:80', 'Ataque:105', 'Ataque especial:60', 'Defesa:65', 'Defesa especial:75', 'Velocidade:130', 'Total:515'],
-        'habilidade':['Lançamento de Rocha', 'Asa de Aço, Mordida', 'Deslize de Pedras', 'Poder da Terra'],
+        'habilidade':['Asa de Aço', 'Mordida', 'Deslize de Pedras', 'Poder da Terra'],
         'tipo':['#142', 'Pedra', 'Vôo','icone/aerodactyl-142.png'],
     },
 
     'bulbasaur': {
         'status':['Hp:45', 'Ataque:49', 'Ataque especial:65', 'Defesa:49', 'Defesa especial:65', 'Velocidade:45', 'Total:318'],
-        'habilidade':['semente sanguessuga', 'Chicote de vinha', 'Folha navalha', 'Dança de petalas', 'Bomba de sementes', 'Doce incenso', 'Sintese', 'Raio solar'],
+        'habilidade':['Chicote de vinha', 'Folha navalha', 'Bomba de sementes','Raio solar'],
         'tipo':['#01', 'Grama', 'Tóxico', 'icone/bulbasaur-001.png'],
     },
 
     'charmander': {
         'status':['Hp:39', 'Ataque:52', 'Ataque especial:60', 'Defesa:43', 'Defesa especial:50', 'Velocidade:65', 'Total:309'],
-        'habilidade':['Lançar partículas de chamas', 'Disparar um jato de fogo', 'Reduzir a defesa do oponente', 'Energizar a garra para um corte poderoso', 'Respirar fogo'],
+        'habilidade':['Lança-chamas', 'Giro de fogo', 'Arranhar', 'Sopro do dragão'],
         'tipo':['#04', 'Fogo','icone/charmander-004.png' ],
     },
 
@@ -32,61 +32,61 @@ pokemon = {
 
     'cubone': {
         'status':['Hp:50', 'Ataque:50', 'Ataque especial:40', 'Defesa:95', 'Defesa especial:50', 'Velocidade:35', 'Total:320'],
-        'habilidade':['Borda dupla', 'Energia de foco', 'Arração', 'Ossos' ],
-        'tipo':['#104', 'Terreno'],
+        'habilidade':['Borda dupla', 'Energia de foco', 'Arranhão', 'Corrida de ossos' ],
+        'tipo':['#104', 'Terreno', 'icone/cubone-104.png'],
     },
 
     'mew': {
         'status':['Hp:100', 'Ataque:100', 'Ataque especial:100', 'Defesa:100', 'Defesa especial:100', 'Velocidade:100', 'Total:600'],
         'habilidade':['Psiquico', 'Esfera da aura', 'Transformar', 'Preso'],
-        'tipo':['#151', 'Psiquico'],
+        'tipo':['#151', 'Psiquico', 'icone/mew-151.png'],
     },
 
     'mewtwo': {
         'status':['Hp:106', 'Ataque:110', 'Ataque especial:154', 'Defesa:90', 'Defesa especial:90', 'Velocidade:130', 'Total:680'],
         'habilidade':['Visão do futuro', 'Recuperar', 'Névoa', 'Troca de energia'],
-        'tipo':['#160', 'Psiquico'],
+        'tipo':['#160', 'Psiquico', 'icone/mewtwo-150.png'],
     },
 
     'pikachu': {
         'status':['Hp:35', 'Ataque:55', 'Ataque especial:50', 'Defesa:40', 'Defesa especial:50', 'Velocidade:90', 'Total:320'],
         'habilidade':['Trovão', 'Tela de luz', 'thunderbolt', 'Descarga'],
-        'tipo':['#025', 'Eletrico'],
+        'tipo':['#025', 'Eletrico', 'icone/pikachu-25.png'],
     },
 
     'squitle': {
         'status':['Hp:44', 'Ataque:48', 'Ataque especial:50', 'Defesa:65', 'Defesa especial:64', 'Velocidade:43', 'Total:314'],
         'habilidade':['Onda crash', 'Bomba hidráulica', 'Defesa de ferro', 'Calda aqua'],
-        'tipo':['#07', 'Água'],
+        'tipo':['#07', 'Água', 'icone/squitle-007.png'],
     },
 
     'umbreon': {
         'status':['Hp:95', 'Ataque:65', 'Ataque especial:60', 'Defesa:110', 'Defesa especial:130', 'Velocidade:65', 'Total:525'],
         'habilidade':['Ultimo recurso', 'Olhar maldoso', 'Screech', 'Pulso escuro'],
-        'tipo':['#197', 'Sombrio'],
+        'tipo':['#197', 'Sombrio', 'icone/umbreon-197.png'],
     },
 
     'vaporeon': {
         'status':['Hp:130', 'Ataque:65', 'Ataque especial:110', 'Defesa:60', 'Defesa especial:95', 'Velocidade:65', 'Total:525'],
         'habilidade':['Ultimo recurso', 'Bomba hidrálica', 'Armadura ácida', 'Água lamacenta'],
-        'tipo':['#134', 'Água'],
+        'tipo':['#134', 'Água', 'icone/vaporeon-134.png'],
     },
 
     'hypno': {
-        'status':['Hp:60', 'Ataque:65', 'Ataque especial:130', 'Defesa:60', 'Defesa especial:75', 'Velocidade:110', 'Total:500'],
-        'habilidade':[],
-        'tipo':[],
+        'status':['Hp:85', 'Ataque:73', 'Ataque especial:73', 'Defesa:70', 'Defesa especial:115', 'Velocidade:67', 'Total:483'],
+        'habilidade':['Choque psíquico', 'Visão do futuro', 'Enredo desagradável', 'Confusão'],
+        'tipo':['#97', 'Psiquico', 'Sombrio', 'icone/hypno-097.png'],
     },
 
     'lapras': {
-        'status':['Hp:60', 'Ataque:65', 'Ataque especial:130', 'Defesa:60', 'Defesa especial:75', 'Velocidade:110', 'Total:500'],
-        'habilidade':[],
-        'tipo':[],
+        'status':['Hp:130', 'Ataque:85', 'Ataque especial:85', 'Defesa:80', 'Defesa especial:95', 'Velocidade:60', 'Total:535'],
+        'habilidade':['Canção perecer', 'Frio', 'Feixe de gelo', 'Surfar'],
+        'tipo':['#131', 'Água', 'Gelo', 'icone/lapras-131.png'],
     },
 
     'scyther': {
-        'status':['Hp:60', 'Ataque:65', 'Ataque especial:130', 'Defesa:60', 'Defesa especial:75', 'Velocidade:110', 'Total:500'],
-        'habilidade':[],
-        'tipo':[],
+        'status':['Hp:70', 'Ataque:110', 'Ataque especial:55', 'Defesa:80', 'Defesa especial:80', 'Velocidade:105', 'Total:500'],
+        'habilidade':['Dança das espadas', 'X-Scissor', 'Barra de ar', 'Agilidade'],
+        'tipo':['#123', 'Inseto', 'Vôo', 'icone/scyther-123.png'],
     },
 }
